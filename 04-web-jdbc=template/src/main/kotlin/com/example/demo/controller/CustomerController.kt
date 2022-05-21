a9eb76955dfd6c078e6e710cd45137cd014c4dad
Msksgm <msksgm3124@gmail.com>
2022-05-21 10:39:18 +0900

    add: customer table から全てのレコードを取得する API を追加

---
+++ 04-web-jdbc=template/src/main/kotlin/com/example/demo/controller/CustomerController.kt
@@ -0,0 +1,16 @@
+package com.example.demo.controller
+
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("customers")
+class CustomerController(val jdbcTemplate: JdbcTemplate) {
+    @GetMapping("")
+    fun getAllCustomers(): List<Customer> {
+        return SelectAllQueryImpl(jdbcTemplate).perform()
+    }
+}
---
+++ 04-web-jdbc=template/src/main/kotlin/com/example/demo/controller/SelectAllQuery.kt
@@ -0,0 +1,20 @@
+package com.example.demo.controller
+
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Component
+
+interface SelectAllQuery {
+    fun perform(): List<Customer>
+}
+
+@Component
+class SelectAllQueryImpl(val jdbcTemplate: JdbcTemplate): SelectAllQuery {
+    val sql = """
+        SELECT id, first_name, last_name
+        FROM customer;
+    """.trimIndent()
+    val mapper = CustomerRowMapper()
+    override fun perform(): List<Customer> {
+        return jdbcTemplate.queryForStream(sql, mapper).toList()
+    }
+}
