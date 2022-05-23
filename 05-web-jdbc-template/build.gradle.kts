f6e09c2de7913f068fbfe1b727b6000859516a1a
Msksgm <msksgm3124@gmail.com>
2022-05-23 19:54:58 +0900

    first commit

---
+++ 05-web-jdbc-template/build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.0"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+
+	// --------------------------------------------------------------------------
+	// ktlint
+	// https://plugins.gradle.org/plugin/org.jlleitschuh.gradle.ktlint
+	// Linter/Formatter
+	// $ ./gradlew ktlintFormat
+	// --------------------------------------------------------------------------
+	id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	//	ホットリロードで使用
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
